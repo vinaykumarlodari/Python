03a04ad005b5edf570aa0b05aaaf3fca5d04ce3f
vinaykumarlodari <31078169+vinaykumarlodari@users.noreply.github.com>
2017-10-11 13:00:25 -0400

    Update splitstring.py

--- splitstring.py
+++ splitstring.py
@@ -16,10 +16,3 @@ def delimPos(passString):
 			
 print("First Half :",splitString("World is big,Vinay"))
 
-def delimPos(passString):
-    for eachchar in range(len(passString)):
-        if passString[eachchar] == ",":
-            x = eachchar
-            return x
-    
-print("Index is:",delimPos("Helloooo,d"))
