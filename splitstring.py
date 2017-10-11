070615b9125da09c42be970d8a9f850fd3fc5d06
vinaykumarlodari <31078169+vinaykumarlodari@users.noreply.github.com>
2017-10-11 12:29:59 -0400

    Update splitstring.py

--- splitstring.py
+++ splitstring.py
@@ -1,3 +1,21 @@
+def splitString(nuString):
+    dp=delimPos(nuString)
+    word1 =""
+    print(len(nuString))
+    for indx in range(len(nuString)):
+        if indx < dp :
+            word1 = word1 + nuString[indx]
+    return word1
+    
+
+def delimPos(passString):
+    for eachchar in range(len(passString)):
+        if passString[eachchar] == ",":
+            x = eachchar
+            return x
+			
+print("First Half :",splitString("World is big,Vinay"))
+
 def delimPos(passString):
     for eachchar in range(len(passString)):
         if passString[eachchar] == ",":
