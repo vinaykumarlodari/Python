5204a3b599dc2349ed4eb3a874c71dd742094891
vinaykumarlodari <31078169+vinaykumarlodari@users.noreply.github.com>
2017-10-12 10:36:15 -0400

    Update Turtlerace.py

--- Turtlerace.py
+++ Turtlerace.py
@@ -1,21 +1,21 @@
-import turtle              # 1.  import the modules
-from random import randint
-wn = turtle.Screen()       # 2.  Create a screen
-wn.bgcolor('lightblue')
+import turtle              # import the modules
+from random import randint # to make the random movement of turtles use ranindt() from random module
+wn = turtle.Screen()       # Create a screen
+wn.bgcolor('yellow')
 
-lance = turtle.Turtle()    # 3.  Create two turtles
+lance = turtle.Turtle()    # Create two turtles
 andy = turtle.Turtle()
 lance.color('red')
 andy.color('blue')
 lance.shape('turtle')
 andy.shape('turtle')
 
-andy.up()                  # 4.  Move the turtles to their starting point
+andy.up()                  # Move the turtles to their starting point
 lance.up()
 andy.goto(-100,20)
 lance.goto(-100,-20)
 
-for i in range(50):
+for i in range(80):
     andy.forward(randint(1,5))
     lance.forward(randint(1,5))
     
